78f677ba5542ff3fe48fc6731106b0dc297ec16d
martyr-rrr <novosadova.sofia007@mail.ru>
2025-11-03 14:12:25 +0000

    Add Bootstrap templates for main page with form

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, url_for
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,4 +9,10 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def index():
-    return 'Hello, Hexlet!'
+    return render_template('index.html')
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    url = request.form.get('url')
+    # Пока просто возвращаем заглушку
+    return f"Анализ URL: {url} - функция в разработке"
